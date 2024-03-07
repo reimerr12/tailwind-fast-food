444ff9aa8919f662753e494cf18e2e8f9c82fb6f
reimerr12 <sameermahirmomo@gmail.com>
2024-03-07 16:16:17 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,60 @@
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+const closeIcon = document.getElementById('nav-close');
+const navLink = document.querySelectorAll(".nav__link");
+
+hamburger.addEventListener("click", () => {
+    navMenu.classList.remove('hidden')
+})
+
+closeIcon.addEventListener("click", () => {
+    navMenu.classList.add('hidden')
+})
+
+navLink.forEach(link =>
+    link.addEventListener("click", () => {
+        navMenu.classList.add('hidden')
+    })
+)
+
+/* `````````````````Tabs`````````````````` */
+
+const tabs = document.querySelectorAll(".tabs_wrap ul li");
+const all = document.querySelectorAll(".item_wrap");
+const foods = document.querySelectorAll(".food");
+const beverages = document.querySelectorAll(".beverage");
+const snacks = document.querySelectorAll(".snack");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+
+    tab.classList.add("active");
+
+    const tabval = tab.getAttribute("data-tabs");
+
+    all.forEach((item) => {
+      item.style.display = "none";
+    });
+
+    if (tabval == "food") {
+      foods.forEach((item) => {
+        item.style.display = "block";
+      });
+    } else if (tabval == "snack") {
+      snacks.forEach((item) => {
+        item.style.display = "block";
+      });
+    } else if (tabval == "beverage") {
+      beverages.forEach((item) => {
+        item.style.display = "block";
+      });
+    } else {
+      all.forEach((item) => {
+        item.style.display = "block";
+      });
+    }
+  });
+});
